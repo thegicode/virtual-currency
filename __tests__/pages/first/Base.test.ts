ad6f8124815a374a6be8a61b53ec8a76d3f406ce
thegicode <thegi.code@gmail.com>
2024-02-10 23:54:34 +0900

    add test

--- __tests__/pages/first/Base.test.ts
+++
@@ -1,8 +0,0 @@
-import Base from "../../../src/scripts/pages/first/Base";
-// import Base from "@pages/first/Base";
-
-const base = new Base("a");
-
-test("log name", () => {
-    expect(base.log()).toBe("a");
-});
--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
 export const presets = [
     ["@babel/preset-env", { targets: { node: "current" } }],
     "@babel/preset-typescript",
+    "es2015",
 ];
