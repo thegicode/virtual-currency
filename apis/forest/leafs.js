41b1124bef0232e89b7d90575ce29de996d4d8cc
thegicode <thegi.code@gmail.com>
2024-02-25 20:11:22 +0900

    update

--- apis/forest/leafs.js
+++ apis/forest/leafs.js
@@ -9,7 +9,7 @@ module.exports = function leafs() {
     let index = 0;
 
     tree();
-    console.log("tree", index, new Date());
+    console.log("tree", index, formatTime(new Date()));
 
     treeTimer();
 
@@ -17,7 +17,21 @@ module.exports = function leafs() {
         setInterval(() => {
             tree();
             index++;
-            console.log("tree", index, new Date());
+            console.log("tree", index, formatTime(new Date()));
         }, 1800000);
+    }
+
+    function formatTime(date) {
+        const options = {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: false,
+            timeZone: "Asia/Seoul",
+        };
+        return new Intl.DateTimeFormat("ko-KR", options).format(date);
     }
 };
