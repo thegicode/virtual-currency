425abbd5790d0b620236cf35466e44859a81b99e
thegicode <thegi.code@gmail.com>
2024-05-14 17:28:22 +0900

    udpate backtest8

--- dev/scripts/pages/backtest5 복사본/index.js
+++
@@ -1,9 +0,0 @@
-import AppBacktest5 from "./AppBacktest5";
-import Overview from "./Overview";
-import Control from "./Control";
-import Table from "./Table";
-customElements.define("backtest-table", Table);
-customElements.define("backtest-control", Control);
-customElements.define("backtest-overview", Overview);
-customElements.define("app-backtest5", AppBacktest5);
-//# sourceMappingURL=index.js.map
